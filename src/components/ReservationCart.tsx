3178ab2cd6d330d0d20d45068215b9ef3346a7e6
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-03-24 19:27:35 +0700

    Fix user error in MyBookingPage

--- src/components/ReservationCart.tsx
+++ src/components/ReservationCart.tsx
@@ -15,8 +15,12 @@ import getUsers from "@/libs/Users/getUsers";
 
 const ReservationCart = () => {
     const dispatch = useDispatch<AppDispatch>();
-    const reservationArr = useSelector((state: RootState) => state.cart.reservationItems);
+    let reservationArr = useSelector((state: RootState) => state.cart.reservationItems);
     const userArr = useSelector((state: RootState) => state.user.user);
+
+    const { data: session } = useSession();
+    const token = session?.user.token;
+    const [loading, setLoading] = useState(true);
 
     const connectedReservations = reservationArr.map((reservation) => {
         const userInfo = userArr.find((user) => user._id === reservation.user);
@@ -26,9 +30,7 @@ const ReservationCart = () => {
         };
       });
 
-    const { data: session } = useSession();
-    const token = session?.user.token;
-    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,10 +38,9 @@ const ReservationCart = () => {
                 const response = await getReservations(token);
                 dispatch(fetchReservation(response.data));
             }
-            if(userArr.length == 0 && token){
+            if(userArr.length == 0 && token && session?.user.role == 'admin'){
                 const response = await getUsers(token);
                 dispatch(fetchUsers(response.data));
-                console.log("FETCH")
             }
             setLoading(false);
         };
@@ -73,7 +74,11 @@ const ReservationCart = () => {
                 <div className="bg-white shadow-md rounded-lg p-5 border border-gray-200" key={connectedReservation._id}>
                     <div className="text-gray-700 text-sm">{connectedReservation.date}</div>
                     {connectedReservation.shop && <div className="text-gray-900 font-medium text-base">{connectedReservation.shop.name}</div>}
-                    <div className="text-gray-900 font-medium text-base">{connectedReservation.userName}</div>
+                    {
+                        session?.user.role == 'admin' 
+                        ? <div className="text-gray-900 font-medium text-base">{connectedReservation.userName}</div> 
+                        : <div className="text-gray-900 font-medium text-base">{session?.user.name}</div>
+                    }
                     <div className="flex space-x-3 mt-3">
                         <button
                             className="bg-red-500 text-white py-1.5 px-4 rounded-lg hover:bg-red-600 transition"
